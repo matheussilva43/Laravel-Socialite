bbf04d91454116328718ec8878f43b73f4dbfc8b
Matheus Silva <msmatheuspereira@gmail.com>
2018-09-17 16:29:36 -0300

    correção no tamanho mensagem

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
     <div class="container">
-        <div class="box-body">
-            @if(session('error'))
-                <div class="alert alert-danger alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                    <h4><i class="icon fa fa-ban"></i> Alerta!</h4>
-                    {{ session('error') }}
-                </div>
-            @endif
-
-            @if(session('success'))
-                <div class="alert alert-success alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                    <h4><i class="icon fa fa-check"></i> Alerta!</h4>
-                    {{ session('success') }}
-                </div>
-            @endif
-        </div>
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <div class="box-body">
+                    @if(session('error'))
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-ban"></i> Alerta!</h4>
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
+                    @if(session('success'))
+                        <div class="alert alert-success alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-check"></i> Alerta!</h4>
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">Login</div>
 
