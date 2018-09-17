e3f343bdafad7aa95b5081b3066e02bc3199de94
Matheus Silva <msmatheuspereira@gmail.com>
2018-09-17 16:19:55 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+#Laravel 5.5 - Socialite
+
+* Consulte a documentação oficial do Laravel
+
+        https://laravel.com/docs/5.5/socialite
+
+   Laravel 5.5 Socialite : Login with Twitter, Facebook, Google, Github, Linkedin
+   
+   * Login do Facebook usando Socialite:
+   
+   Registre-se em.
+   
+        developers.facebook.com
+   
+   * Login do Google + usando o Socialite:
+
+   Primeiro, você precisa criar um novo projeto em
+
+        console.developers.google.com
+
+   * Login do Twitter usando o Socialite:
+   
+   Registre um novo aplicativo em
+   
+        apps.twitter.com
+
+   * Login do Github usando Socialite:
+   
+            * Faça login na sua conta do github e vá para as configurações.
+
+            * Clique em "Aplicativos OAuth" no menu "Configurações do desenvolvedor" no lado esquerdo.
+            
+            * Registre um novo aplicativo.
+            
+            * Você receberá seu ID de cliente e segredo do cliente, coloque-os em seus aplicativos e faça referência a eles config/services.php.
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="box-body">
+            @if(session('error'))
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-ban"></i> Alerta!</h4>
+                    {{ session('error') }}
+                </div>
+            @endif
+
+            @if(session('success'))
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-check"></i> Alerta!</h4>
+                    {{ session('success') }}
+                </div>
+            @endif
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Login</div>
+
+                    <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Login
+                                    </button>
+
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        Forgot Your Password?
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-4 control-label">Or login with</label>
+                                <div class="row">
+                                </div>
+                                <div class="col-md-8 col-md-offset-2">
+                                    <a href="{{ url('login/twitter') }}" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i> Twitter</a>
+                                    <a href="{{ url('login/facebook') }}" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i> Facebook</a>
+                                    <a href="{{ url('login/google') }}" class="btn btn-social-icon btn-google-plus"><i class="fa fa-google-plus"></i> Google</a>
+                                    <a href="{{ url('login/linkedin') }}" class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i> Linkedin</a>
+                                    <a href="{{ url('login/github') }}" class="btn btn-social-icon btn-github"><i class="fa fa-github"></i> Github</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
